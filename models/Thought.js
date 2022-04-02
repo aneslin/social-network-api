60202a71e42f80f5f452f0089bcd47ca20d9422e
Aaron Neslin <aneslin125@gmail.com>
2022-04-02 16:15:24 -0400

    user model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,17 @@
 const { Schema, model } = require("mongoose");
+
+
+const ThoughtSchema = new Schema(
+    thoughtText:{
+        type:String,
+        require:true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt:{
+        type: Date,
+        require:true
+
+    }
+
+)
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema)
+
+model.exports = User
