63ba6e7a1084bddf74296dc0bda5ccf7f5441130
Aaron Neslin <aneslin125@gmail.com>
2022-04-02 16:51:56 -0400

    thought and reaction model and dateformat

--- models/Thought.js
+++ models/Thought.js
@@ -1,17 +1,70 @@
 const { Schema, model } = require("mongoose");
+const dateformat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+    {
+        reactionId:{
+            type: Schema.Types.ObjectId,
+            default: ()=> new Types.ObjectId()
+        },
+
+        reactionBody:{
+            type:String,
+            required:true,
+            maxLength:280
+        },
+
+        username:{
+            type: String,
+            required:true
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: (createdAtVal) => dateFormat(createdAtVal),
+          },
+    },
+
+    {
+        toJSON: {
+          getters: true,
+        },
+      }
+)
+
 
 
 const ThoughtSchema = new Schema(
-    thoughtText:{
-        type:String,
-        require:true,
-        minLength: 1,
-        maxLength: 280
+  {
+    thoughtText: {
+      type: String,
+      require: true,
+      minLength: 1,
+      maxLength: 280,
     },
-    createdAt:{
-        type: Date,
-        require:true
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
 
-    }
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJson: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
 
-)
+ThoughtSchema.virtual("ReactionCount").get(function () {
+  return this.reaction.length;
+});
+
+const Thoughts = model("Thoughts", ThoughtSchema)
