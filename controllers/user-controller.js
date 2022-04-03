fe4c6fdfcc4c78f047a4bb2d016305e06d2518e3
Aaron Neslin <aneslin125@gmail.com>
2022-04-02 21:03:23 -0400

    added user update

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -39,6 +39,23 @@ const userController = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
   },
-};
+
+
+//update user
+updateUser({ params, body }, res) {
+  User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "no pizza found with this id" });
+        return
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => res.status(400).json(err));
+  }
+
+  
+
+}
 
 module.exports = userController;
